dc6bc80be55ef60f1c296713f4d3eded43562996
Marques Avery <averymarques4@gmail.com>
2020-08-27 20:03:43 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Backer
+
+  attr_accessor :name, :backed_projects
+  # attr_reader :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    self.backed_projects << project
+    project.backers << self
+  end
+
+end
