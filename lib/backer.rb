5a9ff0ad6859152e91d4edeb507c7de81a01efb8
Marques Avery <averymarques4@gmail.com>
2020-08-27 19:55:24 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Backer
+
+  attr_accessor :name, :backed_projects
+  # attr_reader :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    # binding.pry
+    self.backed_projects << project
+    self.backed_projects.last.backers << self.name
+  end
+
+end
